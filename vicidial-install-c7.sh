c48a98d478deee7ae92c6411c8b1cbac4fa3332a
GenXoutsourcing <95140975+GenXoutsourcing@users.noreply.github.com>
2021-12-18 11:28:03 -0500

    Update vicidial-install-c7.sh

--- vicidial-install-c7.sh
+++ vicidial-install-c7.sh
@@ -4,8 +4,62 @@ echo "Vicidial installation Centos7 with WebPhone(WebRTC/SIP.js)"
 
 export LC_ALL=C
 
+yum groupinstall "Development Tools" -y
+
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum -y install yum-utils
+yum-config-manager --enable remi-php74
+
 yum install make patch gcc perl-Term-ReadLine-Gnu gcc-c++ subversion php php-devel php-gd gd-devel php-mbstring php-mcrypt php-imap php-ldap php-mysql php-odbc php-pear php-xml php-xmlrpc curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel httpd libpcap libpcap-devel libnet ncurses ncurses-devel screen mysql-devel ntp mutt glibc.i686 wget nano unzip sipsak sox libss7* libopen* openssl libsrtp libsrtp-devel unixODBC unixODBC-devel libtool-ltdl libtool-ltdl-devel -y
 yum -y install sqlite-devel
+
+
+tee -a /etc/httpd/conf/httpd.conf <<EOF
+
+CustomLog /dev/null common
+
+Alias /RECORDINGS/MP3 "/var/spool/asterisk/monitorDONE/MP3/"
+
+<Directory "/var/spool/asterisk/monitorDONE/MP3/">
+    Options Indexes MultiViews
+    AllowOverride None
+    Require all granted
+</Directory>
+
+<VirtualHost *:80>
+    ServerName other.example.com
+</VirtualHost>
+EOF
+
+
+tee -a /etc/php.ini <<EOF
+
+error_reporting  =  E_ALL & ~E_NOTICE
+memory_limit = 448M
+short_open_tag = On
+max_execution_time = 3330
+max_input_time = 3360
+post_max_size = 448M
+upload_max_filesize = 442M
+default_socket_timeout = 3360
+date.timezone = America/New_York
+EOF
+
+
+systemctl restart httpd
+
+
+cat <<MARIADB>> /etc/yum.repos.d/MariaDB.repo
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1
+
+MARIADB
+
+
 yum install mariadb-server mariadb -y
 
 cp /etc/my.cnf /etc/my.cnf.original
@@ -175,17 +229,6 @@ perl Makefile.PL
 make all
 make install 
 
-#Install SIPSack
-
-cd /usr/src
-wget http://download.vicidial.com/required-apps/sipsak-0.9.6-1.tar.gz
-tar -zxf sipsak-0.9.6-1.tar.gz
-cd sipsak-0.9.6
-./configure
-make
-make install
-/usr/local/bin/sipsak --version
-
 
 #Install Lame
 cd /usr/src
@@ -207,29 +250,6 @@ make clean
 make
 make install 
 ldconfig
-
-cd /usr/src
-wget https://github.com/eaccelerator/eaccelerator/zipball/master -O eaccelerator.zip
-unzip eaccelerator.zip
-cd eaccelerator-*
-export PHP_PREFIX="/usr"
-$PHP_PREFIX/bin/phpize
-./configure --enable-eaccelerator=shared --with-php-config=$PHP_PREFIX/bin/php-config
-make
-make install
-
-#Change PHP config
-
-echo "Download the PHP ini file from Git"
-wget -O /etc/php.ini https://raw.githubusercontent.com/jaganthoutam/vicidial-install-scripts/main/php.ini
-
-mkdir /tmp/eaccelerator
-chmod 0777 /tmp/eaccelerator
-php -v
-
-echo "Donwload httpd.cof file from git"
-wget -O /etc/httpd/conf/httpd.conf https://raw.githubusercontent.com/jaganthoutam/vicidial-install-scripts/main/httpd.conf
-
 
 #Install Dahdi
 echo "Install Dahdi"
@@ -262,7 +282,7 @@ tar -xvzf libpri-*
 cd /usr/src/asterisk/asterisk*
 
 : ${JOBS:=$(( $(nproc) + $(nproc) / 2 ))}
-./configure --libdir=/usr/lib --with-gsm=internal --enable-opus --enable-srtp --with-ssl --enable-asteriskssl --with-pjproject-bundled
+./configure --libdir=/usr/lib --with-gsm=internal --enable-opus --enable-srtp --with-ssl --enable-asteriskssl --with-pjproject-bundled --with-jansson-bundled
 
 make menuselect/menuselect menuselect-tree menuselect.makeopts
 #enable app_meetme
@@ -313,16 +333,86 @@ read -p 'Press Enter to continue: '
 echo 'Continuing...'
 
 #Get astguiclient.conf file
-echo "" > /etc/astguiclient.conf
-wget -O /etc/astguiclient.conf https://raw.githubusercontent.com/jaganthoutam/vicidial-install-scripts/main/astguiclient.conf
+cat <<ASTGUI>> /etc/astguiclient.conf
+# astguiclient.conf - configuration elements for the astguiclient package
+# this is the astguiclient configuration file
+# all comments will be lost if you run install.pl again
+
+# Paths used by astGUIclient
+PATHhome => /usr/share/astguiclient
+PATHlogs => /var/log/astguiclient
+PATHagi => /var/lib/asterisk/agi-bin
+PATHweb => /var/www/html
+PATHsounds => /var/lib/asterisk/sounds
+PATHmonitor => /var/spool/asterisk/monitor
+PATHDONEmonitor => /var/spool/asterisk/monitorDONE
+
+# The IP address of this machine
+VARserver_ip => SERVERIP
+
+# Database connection information
+VARDB_server => localhost
+VARDB_database => asterisk
+VARDB_user => cron
+VARDB_pass => 1234
+VARDB_custom_user => custom
+VARDB_custom_pass => custom1234
+VARDB_port => 3306
+
+# Alpha-Numeric list of the astGUIclient processes to be kept running
+# (value should be listing of characters with no spaces: 123456)
+#  X - NO KEEPALIVE PROCESSES (use only if you want none to be keepalive)
+#  1 - AST_update
+#  2 - AST_send_listen
+#  3 - AST_VDauto_dial
+#  4 - AST_VDremote_agents
+#  5 - AST_VDadapt (If multi-server system, this must only be on one server)
+#  6 - FastAGI_log
+#  7 - AST_VDauto_dial_FILL (only for multi-server, this must only be on one server)
+#  8 - ip_relay (used for blind agent monitoring)
+#  9 - Timeclock auto logout
+#  E - Email processor, (If multi-server system, this must only be on one server)
+#  S - SIP Logger (Patched Asterisk 13 required)
+VARactive_keepalives => 1234568ES
+
+# Asterisk version VICIDIAL is installed for
+VARasterisk_version => 13.X
+
+# FTP recording archive connection information
+VARFTP_host => 10.0.0.4
+VARFTP_user => cron
+VARFTP_pass => test
+VARFTP_port => 21
+VARFTP_dir => RECORDINGS
+VARHTTP_path => http://10.0.0.4
+
+# REPORT server connection information
+VARREPORT_host => 10.0.0.4
+VARREPORT_user => cron
+VARREPORT_pass => test
+VARREPORT_port => 21
+VARREPORT_dir => REPORTS
+
+# Settings for FastAGI logging server
+VARfastagi_log_min_servers => 3
+VARfastagi_log_max_servers => 16
+VARfastagi_log_min_spare_servers => 2
+VARfastagi_log_max_spare_servers => 8
+VARfastagi_log_max_requests => 1000
+VARfastagi_log_checkfordead => 30
+VARfastagi_log_checkforwait => 60
+
+# Expected DB Schema version for this install
+ExpectedDBSchema => 1645
+ASTGUI
+
 echo "Replace IP address in Default"
 echo "%%%%%%%%%Please Enter This Server IP ADD%%%%%%%%%%%%"
 read serveripadd
 sed -i s/SERVERIP/"$serveripadd"/g /etc/astguiclient.conf
 
 echo "Install VICIDIAL"
-echo "Copy sample configuration files to /etc/asterisk/ SET TO  Y*"
-perl install.pl
+perl install.pl --no-prompt --copy_sample_conf_files=Y
 
 #Secure Manager 
 sed -i s/0.0.0.0/127.0.0.1/g /etc/asterisk/manager.conf
@@ -332,17 +422,218 @@ echo "Populate AREA CODES"
 echo "Replace OLD IP. You need to Enter your Current IP here"
 /usr/share/astguiclient/ADMIN_update_server_ip.pl --old-server_ip=10.10.10.15
 
+
+perl install.pl --no-prompt
+
+
 #Install Crontab
-wget -O /root/crontab-file https://raw.githubusercontent.com/jaganthoutam/vicidial-install-scripts/main/crontab
+cat <<CRONTAB>> /root/crontab-file
+#copy paste the below entries
+
+### recording mixing/compressing/ftping scripts
+#0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57 * * * * /usr/share/astguiclient/AST_CRON_audio_1_move_mix.pl
+0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57 * * * * /usr/share/astguiclient/AST_CRON_audio_1_move_mix.pl --MIX
+0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57 * * * * /usr/share/astguiclient/AST_CRON_audio_1_move_VDonly.pl
+1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52,55,58 * * * * /usr/share/astguiclient/AST_CRON_audio_2_compress.pl --MP3
+#2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59 * * * * /usr/share/astguiclient/AST_CRON_audio_3_ftp.pl --GSM
+
+### keepalive script for astguiclient processes
+* * * * * /usr/share/astguiclient/ADMIN_keepalive_ALL.pl --cu3way
+
+### kill Hangup script for Asterisk updaters
+* * * * * /usr/share/astguiclient/AST_manager_kill_hung_congested.pl
+
+### updater for voicemail
+* * * * * /usr/share/astguiclient/AST_vm_update.pl
+
+### updater for conference validator
+* * * * * /usr/share/astguiclient/AST_conf_update.pl
+
+### flush queue DB table every hour for entries older than 1 hour
+11 * * * * /usr/share/astguiclient/AST_flush_DBqueue.pl -q
+
+### fix the vicidial_agent_log once every hour and the full day run at night
+33 * * * * /usr/share/astguiclient/AST_cleanup_agent_log.pl
+50 0 * * * /usr/share/astguiclient/AST_cleanup_agent_log.pl --last-24hours
+## uncomment below if using QueueMetrics
+#*/5 * * * * /usr/share/astguiclient/AST_cleanup_agent_log.pl --only-qm-live-call-check
+
+## uncomment below if using Vtiger
+#1 1 * * * /usr/share/astguiclient/Vtiger_optimize_all_tables.pl --quiet
+
+### updater for VICIDIAL hopper
+* * * * * /usr/share/astguiclient/AST_VDhopper.pl -q
+
+### adjust the GMT offset for the leads in the vicidial_list table
+1 1,7 * * * /usr/share/astguiclient/ADMIN_adjust_GMTnow_on_leads.pl --debug
+
+### reset several temporary-info tables in the database
+2 1 * * * /usr/share/astguiclient/AST_reset_mysql_vars.pl
+
+### optimize the database tables within the asterisk database
+3 1 * * * /usr/share/astguiclient/AST_DB_optimize.pl
+
+## adjust time on the server with ntp
+30 * * * * /usr/sbin/ntpdate -u pool.ntp.org 2>/dev/null 1>&amp;2
+
+### VICIDIAL agent time log weekly and daily summary report generation
+2 0 * * 0 /usr/share/astguiclient/AST_agent_week.pl
+22 0 * * * /usr/share/astguiclient/AST_agent_day.pl
+
+### VICIDIAL campaign export scripts (OPTIONAL)
+#32 0 * * * /usr/share/astguiclient/AST_VDsales_export.pl
+#42 0 * * * /usr/share/astguiclient/AST_sourceID_summary_export.pl
+### remove old recordings more than 7 days old
+#24 0 * * * /usr/bin/find /var/spool/asterisk/monitorDONE -maxdepth 2 -type f -mtime +7 -print | xargs rm -f
+
+### roll logs monthly on high-volume dialing systems
+#30 1 1 * * /usr/share/astguiclient/ADMIN_archive_log_tables.pl
+
+### remove old vicidial logs and asterisk logs more than 2 days old
+28 0 * * * /usr/bin/find /var/log/astguiclient -maxdepth 1 -type f -mtime +2 -print | xargs rm -f
+29 0 * * * /usr/bin/find /var/log/asterisk -maxdepth 3 -type f -mtime +2 -print | xargs rm -f
+30 0 * * * /usr/bin/find / -maxdepth 1 -name "screenlog.0*" -mtime +4 -print | xargs rm -f
+
+### cleanup of the scheduled callback records
+25 0 * * * /usr/share/astguiclient/AST_DB_dead_cb_purge.pl --purge-non-cb -q
+
+### GMT adjust script - uncomment to enable
+#45 0 * * * /usr/share/astguiclient/ADMIN_adjust_GMTnow_on_leads.pl --list-settings
+
+### Dialer Inventory Report
+1 7 * * * /usr/share/astguiclient/AST_dialer_inventory_snapshot.pl -q --override-24hours
+
+### inbound email parser
+* * * * * /usr/share/astguiclient/AST_inbound_email_parser.pl
+CRONTAB
+
 crontab /root/crontab-file
 crontab -l
 
 #Install rc.local
-> /etc/rc.d/rc.local
-wget -O /etc/rc.d/rc.local https://raw.githubusercontent.com/jaganthoutam/vicidial-install-scripts/main/rc.local
+tee -a /etc/rc.d/rc.local <<EOF
+
+
+# OPTIONAL enable ip_relay(for same-machine trunking and blind monitoring)
+
+/usr/share/astguiclient/ip_relay/relay_control start 2>/dev/null 1>&2
+
+
+# Disable console blanking and powersaving
+
+/usr/bin/setterm -blank
+
+/usr/bin/setterm -powersave off
+
+/usr/bin/setterm -powerdown
+
+
+### start up the MySQL server
+
+systemctl start mariadb.service
+
+
+### start up the apache web server
+
+systemctl start httpd.service
+
+
+### roll the Asterisk logs upon reboot
+
+/usr/share/astguiclient/ADMIN_restart_roll_logs.pl
+
+
+### clear the server-related records from the database
+
+/usr/share/astguiclient/AST_reset_mysql_vars.pl
+
+
+### load dahdi drivers
+
+modprobe dahdi
+modprobe dahdi_dummy
+
+/usr/sbin/dahdi_cfg -vvvvvvvvvvvvv
+
+
+### sleep for 20 seconds before launching Asterisk
+
+sleep 20
+
+
+### start up asterisk
+
+/usr/share/astguiclient/start_asterisk_boot.pl
+EOF
+
 chmod +x /etc/rc.d/rc.local
 systemctl enable rc-local
 systemctl start rc-local
+
+
+##Install Sounds
+
+cd /usr/src
+wget http://downloads.digium.com/pub/telephony/sounds/asterisk-core-sounds-en-ulaw-current.tar.gz
+wget http://downloads.digium.com/pub/telephony/sounds/asterisk-core-sounds-en-wav-current.tar.gz
+wget http://downloads.digium.com/pub/telephony/sounds/asterisk-core-sounds-en-gsm-current.tar.gz
+wget http://downloads.digium.com/pub/telephony/sounds/asterisk-extra-sounds-en-ulaw-current.tar.gz
+wget http://downloads.digium.com/pub/telephony/sounds/asterisk-extra-sounds-en-wav-current.tar.gz
+wget http://downloads.digium.com/pub/telephony/sounds/asterisk-extra-sounds-en-gsm-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-moh-opsound-gsm-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-moh-opsound-ulaw-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-moh-opsound-wav-current.tar.gz
+
+#Place the audio files in their proper places:
+cd /var/lib/asterisk/sounds
+tar -zxf /usr/src/asterisk-core-sounds-en-gsm-current.tar.gz
+tar -zxf /usr/src/asterisk-core-sounds-en-ulaw-current.tar.gz
+tar -zxf /usr/src/asterisk-core-sounds-en-wav-current.tar.gz
+tar -zxf /usr/src/asterisk-extra-sounds-en-gsm-current.tar.gz
+tar -zxf /usr/src/asterisk-extra-sounds-en-ulaw-current.tar.gz
+tar -zxf /usr/src/asterisk-extra-sounds-en-wav-current.tar.gz
+
+mkdir /var/lib/asterisk/mohmp3
+mkdir /var/lib/asterisk/quiet-mp3
+ln -s /var/lib/asterisk/mohmp3 /var/lib/asterisk/default
+
+cd /var/lib/asterisk/mohmp3
+tar -zxf /usr/src/asterisk-moh-opsound-gsm-current.tar.gz
+tar -zxf /usr/src/asterisk-moh-opsound-ulaw-current.tar.gz
+tar -zxf /usr/src/asterisk-moh-opsound-wav-current.tar.gz
+rm -f CHANGES*
+rm -f LICENSE*
+rm -f CREDITS*
+
+cd /var/lib/asterisk/moh
+rm -f CHANGES*
+rm -f LICENSE*
+rm -f CREDITS*
+
+cd /var/lib/asterisk/sounds
+rm -f CHANGES*
+rm -f LICENSE*
+rm -f CREDITS*
+
+
+cd /var/lib/asterisk/quiet-mp3
+sox ../mohmp3/macroform-cold_day.wav macroform-cold_day.wav vol 0.25
+sox ../mohmp3/macroform-cold_day.gsm macroform-cold_day.gsm vol 0.25
+sox -t ul -r 8000 -c 1 ../mohmp3/macroform-cold_day.ulaw -t ul macroform-cold_day.ulaw vol 0.25
+sox ../mohmp3/macroform-robot_dity.wav macroform-robot_dity.wav vol 0.25
+sox ../mohmp3/macroform-robot_dity.gsm macroform-robot_dity.gsm vol 0.25
+sox -t ul -r 8000 -c 1 ../mohmp3/macroform-robot_dity.ulaw -t ul macroform-robot_dity.ulaw vol 0.25
+sox ../mohmp3/macroform-the_simplicity.wav macroform-the_simplicity.wav vol 0.25
+sox ../mohmp3/macroform-the_simplicity.gsm macroform-the_simplicity.gsm vol 0.25
+sox -t ul -r 8000 -c 1 ../mohmp3/macroform-the_simplicity.ulaw -t ul macroform-the_simplicity.ulaw vol 0.25
+sox ../mohmp3/reno_project-system.wav reno_project-system.wav vol 0.25
+sox ../mohmp3/reno_project-system.gsm reno_project-system.gsm vol 0.25
+sox -t ul -r 8000 -c 1 ../mohmp3/reno_project-system.ulaw -t ul reno_project-system.ulaw vol 0.25
+sox ../mohmp3/manolo_camp-morning_coffee.wav manolo_camp-morning_coffee.wav vol 0.25
+sox ../mohmp3/manolo_camp-morning_coffee.gsm manolo_camp-morning_coffee.gsm vol 0.25
+sox -t ul -r 8000 -c 1 ../mohmp3/manolo_camp-morning_coffee.ulaw -t ul manolo_camp-morning_coffee.ulaw vol 0.25
+
+
 
 read -p 'Press Enter to Reboot: '
 
